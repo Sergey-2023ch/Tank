3fce2a9ef03deac832735c375c995b53fec977b5
Maksonina <maksonina1@gmail.com>
2022-10-07 11:41:07 +0300

    First commit

---
+++ src/Tank.java
@@ -0,0 +1,29 @@
+import javax.swing.text.Position;
+
+public class Tank {
+    int x;
+    int i;
+    int goForward = (i);
+    int goBackward = (i);
+
+    public Tank() {
+        this(0);
+    }
+
+    public Tank(int x) {
+        this.x = x;
+    }
+
+    public void goForward(int i) {
+
+        x = x + i;
+    }
+        public void printPosition () {
+            System.out.println("The Tank is at " + x + " now");
+        }
+        public void goBackward ( int i){
+
+            x=x+i;
+
+        }
+}
